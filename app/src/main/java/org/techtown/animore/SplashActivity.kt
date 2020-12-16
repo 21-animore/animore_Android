e5327c324795248b9b97a3384dcf9a69fbe73876
Say-young <seyoung990525@gmail.com>
2020-12-16 22:52:30 +0900

    201217 시연용 데모

--- app/src/main/java/org/techtown/animore/SplashActivity.kt
+++ app/src/main/java/org/techtown/animore/SplashActivity.kt
@@ -11,15 +11,16 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash)    //여기에 splash 넣은 layout 파일 이름 입력하고
 
-        start_splash.playAnimation()
+        start_splash.playAnimation()    //이게 splash 재생하는 코드야!
         start_splash.addAnimatorListener(object: Animator.AnimatorListener {
             override fun onAnimationRepeat(animation: Animator?) {
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                //splash 애니메이션이 종료되면 loginActivity로 넘어감
+                //splash 애니메이션이 종료되면 loginActivity로 자동으로 넘어가도록 해놨어!
+                //언니는 그럼 첫 activity로 이동하게 바꾸면 되겠지!
                 val intent = Intent(this@SplashActivity, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
